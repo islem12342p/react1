456c79b7aec0b35cac75b44e14840f1a83250e47
islem12342p <liomcxx@gmail.com>
2023-11-15 17:14:59 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import GroupExample from './Components/Card'
+import Navba from './Components/Navbar'
+function App() {
+  return (
+    <div className="App">
+      <Navba/>
+    <GroupExample/>
+      
+    </div>
+  );
+}
+
+
+export default App
